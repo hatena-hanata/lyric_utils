37fbc1adbb7e90d6b9a31e15f6423934faf037a6
hatena-hanata <hanatasfp@gmail.com>
2020-08-08 03:37:54 +0900

    複数歌詞サイトに対応

--- project/main.py
+++ project/main.py
@@ -5,25 +5,45 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    # name = "Hello World"
-    # return name
     return render_template('top.html')
+
+
+def scraping_utanet(soup):
+    kashi_area_str = str(soup.find('div', id='kashi_area'))
+    kashi = kashi_area_str.replace('<div id="kashi_area" itemprop="text">', '')
+    kashi = kashi.replace('</div>', '')
+    return kashi
+
+
+def scraping_jlyric(soup):
+    kashi_area_str = str(soup.find('p', id='Lyric'))
+    kashi = kashi_area_str.replace('<p id="Lyric">', '')
+    kashi = kashi.replace('</p>', '')
+    return kashi
+
+
+def scraping_utamap(soup):
+    kashi_area_str = str(soup.find('td', class_='noprint kasi_honbun'))
+    kashi = kashi_area_str.replace('<td class="noprint kasi_honbun" style="padding-left:30px;">', '')
+    kashi = kashi.replace('<!-- 歌詞 end -->', '')
+    kashi = kashi.replace('</td>', '')
+    return kashi
+
 
 @app.route('/result', methods=['POST'])
 def scraping():
     url = request.form['url']
     res = requests.get(url)
     soup = BeautifulSoup(res.content, 'html.parser')
-    kashi_area_str = str(soup.find('div', id='kashi_area'))
-    kashi = kashi_area_str.replace('<div id="kashi_area" itemprop="text">', '')
-    kashi = kashi.replace('</div>', '')
-    return jsonify({'output': Markup(kashi)})
-    # return render_template('res.html', kashi=Markup(kashi))
-    # kashi = kashi_area.text
-    # print(kashi)
-    # kashi = kashi.replace('　', '\n')
-    # return render_template('res.html', kashi=Markup(kashi.replace('\n', '<br>')))
 
-## おまじない
+    if 'uta-net' in url:
+        kashi = scraping_utanet(soup)
+    elif 'j-lyric' in url:
+        kashi = scraping_jlyric(soup)
+    elif 'utamap' in url:
+        kashi = scraping_utamap(soup)
+        print(kashi)
+    return jsonify({'output': kashi})
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
